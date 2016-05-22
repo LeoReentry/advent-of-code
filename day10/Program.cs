68d76317eeb58867786a8a1f4e7c4bc878d68fe2
Leo <leononame@lho.io>
2016-05-22 16:58:05 +0200

    Add solution for day 10

---
+++ day10/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace day10
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			string input = "1113122113";
+			string pattern = @"(\d)\1*";
+			for(int i = 0; i < 50; i++){
+				MatchCollection matches = Regex.Matches (input, pattern);
+				StringBuilder sb = new StringBuilder ();
+				foreach (Match m in matches) {
+					string match = m.ToString ();
+					sb.Append(match.Length.ToString() + match [0]);
+				}
+				input = sb.ToString();
+				Console.WriteLine ("Iteration {0:00}: Length is {1}", i+1, input.Length);
+			}
+		}
+	}
+}
